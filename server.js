34eaf8dbf4504518bb0a7f350104fe8736bc07f7
Joseph Sanchez <josephsanchez@josephs-mbp.local>
2022-12-01 19:28:25 -0800

    why

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true, limit: '50mb'}));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
